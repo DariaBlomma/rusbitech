60958cfaf007233cb03d1e53125944ac1c885551
Daria Kozina <kozina.flowers@yandex.ru>
2021-08-16 22:33:54 +0300

    calc html and css appeared

---
+++ php_test/Cli/task2.php
@@ -0,0 +1,20 @@
+<?php 
+    namespace Cli;
+
+    class Task2 {
+        public function __construct() {}
+
+        public function filter() {
+            $dir = '/datafiles';
+            // $dir = 'C:\Users\Руслан\Downloads';
+            $files = array();
+            // выбираются только английские буквы и цифры, по максиммуму без доп символов
+            foreach(glob($dir . '/[!0-9]*[!a-zA-Z].ixt' ) as $file) {
+            // foreach(glob($dir . '/[!0-9]*[!a-zA-Z].*' ) as $file) {
+                if (is_file($file) && preg_match("/^[a-z0-9]*[^а-яё\s_()-\.]*\.[a-z]{3}$/i", basename($file))) {
+                    $files[] = basename($file);	
+                }
+            }                
+            var_dump($files);
+        }
+    }
